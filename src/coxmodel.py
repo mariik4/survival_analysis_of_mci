a7197513d56a8463b42c479e6ea2a6e73e949043
mariik <m.portnykh@fxdev.sk>
2026-03-27 07:24:00 +0100

    feat: refactor custom cox model and write code for tunning

--- src/coxmodel.py
+++ src/coxmodel.py
@@ -4,6 +4,7 @@ from lifelines import CoxPHFitter
 from sklearn.base import BaseEstimator
 from sklearn.model_selection import GridSearchCV, KFold
 from sklearn.utils.class_weight import compute_sample_weight
+from lifelines.utils import concordance_index
 
 class CoxPH(CoxPHFitter):
     def __init__(self, penalizer=0.0, l1_ratio=0.0, compute_weights=True):
@@ -26,3 +27,12 @@ class CoxPH(CoxPHFitter):
         weights = compute_sample_weight("balanced", weighted_df[event_col])
         weighted_df[self._weights_col] = weights
         return weighted_df
+    
+    def score(self, df, duration_col, event_col):
+        # predict_partial_hazard ranking patients by risk
+        # that mean the higher the risk => the lower probability to surviva
+        # But corcondance index expects scores, so we need to negate the result
+        # in order to receive opposite order
+        hazard_prediction = self.predict_partial_hazard(df)
+        predicted_scores = -hazard_prediction
+        return concordance_index(df[duration_col], predicted_scores, df[event_col])
